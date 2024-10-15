6b10ef53f4cbf7d96b768f35d018c4f55f67f6d4
Binnur KURT <deepcloudlabs@gmail.com>
2024-10-15 11:51:40 +0300

    initial commit

--- customer-service/src/main/java/com/example/crm/service/CustomerService.java
+++ customer-service/src/main/java/com/example/crm/service/CustomerService.java
@@ -2,33 +2,40 @@ package com.example.crm.service;
 
 import java.util.List;
 
-import com.example.crm.document.CustomerDocument;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
+import com.example.crm.document.CustomerDocument;
+import com.example.crm.repository.CustomerDocumentRepository;
+
+@Service
 public class CustomerService {
+	private final CustomerDocumentRepository customerDocumentRepository;
+	
+	public CustomerService(CustomerDocumentRepository customerDocumentRepository) {
+		this.customerDocumentRepository = customerDocumentRepository;
+	}
 
 	public CustomerDocument findById(String identity) {
-		// TODO Auto-generated method stub
-		return null;
+		return customerDocumentRepository.findById(identity).orElseThrow(() -> new IllegalArgumentException("Cannot find the customer (%s)".formatted(identity)));
 	}
 
 	public List<CustomerDocument> findAllByPage(int pageNo, int pageSize) {
-		// TODO Auto-generated method stub
-		return null;
+		return customerDocumentRepository.findAll(PageRequest.of(pageNo, pageSize));
 	}
 
 	public CustomerDocument acquireCustomer(CustomerDocument customer) {
-		// TODO Auto-generated method stub
-		return null;
+		return customerDocumentRepository.insert(customer);
 	}
 
 	public CustomerDocument updateCustomer(CustomerDocument customer) {
-		// TODO Auto-generated method stub
-		return null;
+		return customerDocumentRepository.save(customer);
 	}
 
 	public CustomerDocument releaseCustomer(String identity) {
-		// TODO Auto-generated method stub
-		return null;
+		var customer = customerDocumentRepository.findById(identity).orElseThrow(() -> new IllegalArgumentException("Cannot find the customer (%s)".formatted(identity)));
+		customerDocumentRepository.delete(customer);
+		return customer;
 	}
 
 }
